4328665be6519d050a6a101db7bc5a06ca07f669
Andar <50786504+andaaree@users.noreply.github.com>
2025-11-11 20:09:06 +0700

    fix(auth): fix env call in api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,6 @@
 import axios from 'axios';
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const baseURL = process.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL,
