75a8d7118bcf15b079a98bbd5761f519e6f089a7
Andar <50786504+andaaree@users.noreply.github.com>
2025-11-11 19:44:12 +0700

    refactor(auth): fix main file to call BrowserRouter
    # Fix main.tsx
    
    - modify use ReactDOM render BrowserRouter method instead StrictMode with reactdom client createRoot
    - fix import
    
    # Fix App.tsx
    
    - fix React Component call method instead of return it
    - separate children const component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import Layout from "./components/Layout";
+
+const Comp = (<h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>);
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Layout >
+      {Comp}
+    </Layout>
   );
 }
 
